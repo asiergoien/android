bc886b64a2b308d490a1b36de948308d6d4dd900
asier <asier.goienetxeaet@elorrieta-errekamari.com>
2020-12-09 14:19:41 +0100

    google maps

--- 2.EBAL/Ariketak/Google Maps/app/src/main/java/com/example/googlemaps/MainActivity.java
+++ 2.EBAL/Ariketak/Google Maps/app/src/main/java/com/example/googlemaps/MainActivity.java
@@ -1,38 +1,65 @@
 package com.example.googlemaps;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
-
-
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-
+import android.view.View;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-
-public class MainActivity extends FragmentActivity implements OnMapReadyCallback
- {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+public class MainActivity extends FragmentActivity implements
+        OnMapReadyCallback, GoogleMap.OnMapClickListener {
+    private GoogleMap mapa;
+    private final LatLng oElorrieta = new LatLng(43.283531, -2.965031);
+    @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-// Obtenemos el mapa de forma asíncrona (notificará cuando esté listo)
         SupportMapFragment mapFragment = (SupportMapFragment)
                 getSupportFragmentManager().findFragmentById(R.id.mapa);
         mapFragment.getMapAsync(this);
     }
+    @Override public void onMapReady(GoogleMap googleMap) {
+        mapa = googleMap;
+        mapa.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        mapa.getUiSettings().setZoomControlsEnabled(false); // Oculta loscontroles de zoom.
+                mapa.moveCamera(CameraUpdateFactory.newLatLngZoom(oElorrieta, 15)); //Nivel de zoom con el que queremos que entre.
+                mapa.addMarker(new MarkerOptions()
+                .position(oElorrieta)
+                .title("Elorrieta")
+                .snippet("CIFP Elorrieta-Erreka Mari LHII")
+                .icon(BitmapDescriptorFactory
+                        .fromResource(android.R.drawable.ic_menu_compass))
+                .anchor(0.5f, 0.5f));
+        mapa.setOnMapClickListener(this);
+        if (ActivityCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION) ==
 
-     @Override
-     public void onMapReady(GoogleMap googleMap) {
-         GoogleMap mapa = googleMap;
-         LatLng oElorrieta = new LatLng(43.283531, -2.965031); // Elorrieta 43.283531, -2.965031
-         mapa.addMarker(new MarkerOptions().position(oElorrieta).title("Marker Elorrieta"));
-                 mapa.moveCamera(CameraUpdateFactory.newLatLng(oElorrieta));
+                PackageManager.PERMISSION_GRANTED) {
+            mapa.setMyLocationEnabled(true);
+            mapa.getUiSettings().setCompassEnabled(true);
+        }
+    }
+    public void moveCamera(View view) {
+        mapa.moveCamera(CameraUpdateFactory.newLatLng(oElorrieta));
+    }
+    public void animateCamera(View view) {
+        mapa.animateCamera(CameraUpdateFactory.newLatLng(oElorrieta));
+    }
+    public void addMarker(View view) {
+        mapa.addMarker(new MarkerOptions().position(
+                mapa.getCameraPosition().target));
 
+    }
+    @Override public void onMapClick(LatLng puntoPulsado) {
+        mapa.addMarker(new MarkerOptions().position(puntoPulsado)
+                .icon(BitmapDescriptorFactory
+                        .defaultMarker(BitmapDescriptorFactory.HUE_YELLOW)));
 
-     }
- }
+    }}
